0943531ceb919b122f15469d28ea432d6e4aadb2
Emanuel code <emanuel_4554@outlook.com>
2023-07-23 20:14:01 -0400

    actualizacion y nuevos componentes

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 const iconMenu = document.querySelector(".icon-menu");
 const navMenu = document.querySelector(".nav");
 
-iconMenu.addEventListener("click",() => {
-    navMenu.classList.toggle("active");
-})
+iconMenu.addEventListener('click', ()=>{
+    event.stopPropagation();
+    navMenu.classList.toggle('active')
+   })
 
+window.onclick = function (e) {
+    navMenu.classList.remove('active')
+   }
